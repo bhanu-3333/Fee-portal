94efdee7b7cc5f578f2d353d9de2dcb77ce562f6
bhanu-3333 <bhanuu339@gmail.com>
2026-03-26 20:58:16 +0530

    feat:backend file added

---
+++ backend/models/College.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const collegeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    logo: { type: String },
+    collegeId: { type: String, unique: true, required: true },
+    email: { type: String, required: true, unique: true },
+    address: { type: String },
+    departments: [{
+        name: { type: String, required: true },
+        years: [{ type: String, required: true }] // e.g. ["1st Year", "2nd Year"]
+    }],
+}, { timestamps: true });
+
+module.exports = mongoose.model('College', collegeSchema);
